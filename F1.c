66638d1dd9a7504a757b95b3648703f0b512dccb
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-04-11 22:03:14 +0800

    Update F1.c

--- F1.c
+++ F1.c
@@ -34,13 +34,9 @@ void fillString(char* output, int index, const char buf[101]){
 
 }
 
-// from main.c
-// can't return local variable!
-// char* event2str(char sig, event e, char str[100]){
-    // sprintf(str,"%c$3$%d$%d$%d$%d$%d$%s$%s",sig,e.index,e.teamID,e.holdDay,e.startTime,e.endTime,e.name,e.project);
-    // return str;
-// }
-
+void event2str(char sig, event e, char str[101]){
+    sprintf(str,"%c$3$%d$%d$%d$%d$%d$%s$%s",sig,e.index,e.teamID,e.holdDay,e.startTime,e.endTime,e.name,e.project);
+}
 
 int modTime(int mod, event* targetEvent){
     // mod: from 0 to 37
@@ -72,7 +68,7 @@ int main(int argc,char* argv[]) {
     int Ffd[2][2];   // F -> FF
     char Fbuf[2][101];
 
-    if (pipe(GPfd[0]) < 0 || pipe(GPfd[1]) < 0 || pipe(Ffd[0]) < 0 || pipe(Ffd[1]) < 0){
+    if (pipe(GPfd[0]) < 0 || pipe(GPfd[1]) < 0 || pipe(Ffd[0]) < 0 || pipe(Ffd[1]) < 0) {
         printf("F1: UNEXPECT ERROR!\n");
     }
     close(GPfd[0][1]);
@@ -83,7 +79,7 @@ int main(int argc,char* argv[]) {
 
 // Init
     int pid;
-    int eventUsage;
+    int eventUsage = 0;
 
 // create child pipe
     int f1fd[8][2];     // F1 -> Cx
@@ -91,7 +87,7 @@ int main(int argc,char* argv[]) {
     int cfd[8][2];      // Cx -> F1
     char cbuf[8][101];
     for (int i = 0; i < 8; ++i) {
-        if(pipe(f1fd[i]) < 0 || pipe(cfd[i]) < 8){
+        if (pipe(f1fd[i]) < 0 || pipe(cfd[i]) < 8) {
             printf("F1: Pipe creation error!\n");
             exit(1);
         }
@@ -111,9 +107,9 @@ int main(int argc,char* argv[]) {
  */
     for (int i = 0; i < 8; ++i) {
         pid = fork();
-        if (pid < 0){
+        if (pid < 0) {
             printf("F1: Fork fail!\n");
-        } else if (pid == 0){
+        } else if (pid == 0) {
             int childId = i; // from 0 to 7
             // Write Cx code
         }
@@ -126,66 +122,87 @@ int main(int argc,char* argv[]) {
 
 /*
  * ===========================================================
- * Mode: Client of FF
+ * F Mode: Client of FF
  * ===========================================================
  */
-    while (1){
+    while (1) {
         // get message from FF
         sleep(1);
         int np;
-        np = read(GPfd[0][0],GPbuf[0],101);
-        if (np < 0){
+        np = read(GPfd[0][0], GPbuf[0], 101);
+        if (np <= 0) {
             continue;
         } // no message
         char signal = GPbuf[0][0];
 
         // FF pass F1 a new team/group
-        if (signal == 'G'){
+        if (signal == 'G') {
             team oneTeam;
             char tempStr0[101];
 
-            fillString(tempStr0,1,GPbuf[0]);
-            oneTeam.index = atoi(tempStr0);     // get index
-            fillString(tempStr0,2,GPbuf[0]);
-            oneTeam.manager = atoi(tempStr0);   // get manager index
-            fillString(tempStr0,3,GPbuf[0]);
-            oneTeam.manager = atoi(tempStr0);   // get member1 index
-            fillString(tempStr0,4,GPbuf[0]);
-            oneTeam.manager = atoi(tempStr0);   // get member2 index
-            fillString(tempStr0,5,GPbuf[0]);
-            oneTeam.manager = atoi(tempStr0);   // get member3 index
-            fillString(tempStr0,6,GPbuf[0]);
-            strcpy(oneTeam.name,tempStr0);   // get team name
+            fillString(tempStr0, 1, GPbuf[0]);
+            oneTeam.index = atoi(tempStr0);                        // get index
 
-            teamArr[oneTeam.index] = oneTeam;
+            teamArr[oneTeam.index] = oneTeam;                      // copy to teamArr
+
+            fillString(tempStr0, 2, GPbuf[0]);
+            teamArr[oneTeam.index].manager = atoi(tempStr0);       // get manager index
+
+            fillString(tempStr0, 3, GPbuf[0]);
+            teamArr[oneTeam.index].memberCount = atoi(tempStr0);   // get member count
+
+            for (int i = 0; i < teamArr[oneTeam.index].memberCount; ++i) {
+                fillString(tempStr0, 4 + i, GPbuf[0]);
+                teamArr[oneTeam.index].member[i] = atoi(tempStr0); // get every member index
+            }
+
+            fillString(tempStr0, teamArr[oneTeam.index].memberCount + 4, GPbuf[0]);
+            strcpy(teamArr[oneTeam.index].name, tempStr0);          // get team name
+
+            fillString(tempStr0, teamArr[oneTeam.index].memberCount + 5, GPbuf[0]);
+            strcpy(teamArr[oneTeam.index].project, tempStr0);       // get project name
+
+            // response FF ack
+            Fbuf[0][0] = 'D';
+            write(Ffd[0][1], Fbuf[0], 101);
         }
 
         // FF pass F1 to a new event
-        if (signal == 'E'){
+        if (signal == 'E') {
             event oneEvent;
             char tempStr0[101];
 
-            fillString(tempStr0,1,GPbuf[0]);
+            fillString(tempStr0, 1, GPbuf[0]);
             oneEvent.index = atoi(tempStr0);     // get index
-            fillString(tempStr0,2,GPbuf[0]);
-            oneEvent.teamID = atoi(tempStr0);
-            fillString(tempStr0,3,GPbuf[0]);
-            oneEvent.holdDay = atoi(tempStr0);
-            fillString(tempStr0,4,GPbuf[0]);
-            oneEvent.startTime = atoi(tempStr0);
-            fillString(tempStr0,5,GPbuf[0]);
-            oneEvent.endTime = atoi(tempStr0);
-            fillString(tempStr0,6,GPbuf[0]);
-            strcpy(oneEvent.name,tempStr0);
-            fillString(tempStr0,6,GPbuf[0]);
-            strcpy(oneEvent.project,tempStr0);
 
             eventArr[oneEvent.index] = oneEvent;
-            eventUsage++;
+            eventUsage++;                        // copy to eventArr
+
+            fillString(tempStr0, 2, GPbuf[0]);
+            oneEvent.teamID = atoi(tempStr0);
+
+            fillString(tempStr0, 3, GPbuf[0]);
+            oneEvent.holdDay = atoi(tempStr0);
+
+            fillString(tempStr0, 4, GPbuf[0]);
+            oneEvent.startTime = atoi(tempStr0);
+
+            fillString(tempStr0, 5, GPbuf[0]);
+            oneEvent.endTime = atoi(tempStr0);
+
+            fillString(tempStr0, 6, GPbuf[0]);
+            strcpy(oneEvent.name, tempStr0);
+
+            fillString(tempStr0, 6, GPbuf[0]);
+            strcpy(oneEvent.project, tempStr0);
+
+            // response FF ack
+            Fbuf[0][0] = 'D';
+            write(Ffd[0][1], Fbuf[0], 101);
         }
 
         // Print: Tell every child to print their calendar
-        if (signal == 'P'){
+        if (signal == 'P') {
 
             // log if event is successful
             int eventSuccess[eventUsage];
@@ -195,55 +212,63 @@ int main(int argc,char* argv[]) {
 
             // schedule event i
             for (int i = 0; i < eventUsage; ++i) {
-                int mod = 0;
+                int mod = 0;    // modify times, 0 to 37 -> -18 to +18
 
-                while (mod < 37){
+                while (mod < 37) {
                     int arrangement = 1;
                     char message[101];
-                    for (int j = 0; j < teamArr[i].memberCount; ++j) {
+                    int memberCount = teamArr[eventArr[i].teamID].memberCount;
 
-                        // 1. tell child j to try
-                        event2str('E',eventArr[i],message);
-                        strcpy(f1buf[j],message);
-                        write(f1fd[j][1], f1buf[j],101);
-
-                        // 2. listen child j's feedback
-                        while(1){
+                    // for each child index c
+                    for (int j = 0; j < memberCount; ++j) {
+                        int c = teamArr[eventArr[i].teamID].member[j];
+                        // 1. tell child c to try
+                        event2str('E', eventArr[i], message);
+                        strcpy(f1buf[c], message);
+                        write(f1fd[c][1], f1buf[c], 101);
+                        // 2. listen child c's feedback
+                        while (1) {
                             sleep(1);
-                            np = read(cfd[j][0],cbuf[j],101);
-                            if (np < 0){
-                                continue;}
-                            if (cbuf[j][0] == 'N'){
+                            np = read(cfd[c][0], cbuf[c], 101);
+                            if (np <= 0) {
+                                continue;
+                            }
+                            if (cbuf[j][0] == 'N') {
                                 arrangement = 0;
                                 break;
-                            } else if (cbuf[j][0] == 'Y'){
+                            } else if (cbuf[j][0] == 'Y') {
                                 break;
                             }
                         }
+                    }
 
-                        // 3. if all feedbacks say available, tell child do and break
-                        if (arrangement == 1){
-                            while (1){
-                                sleep(1);
-                                // 1. tell child j to add
-                                write(f1fd[j][1], f1buf[j],101);
-                                // 2. wait ack
-                                if (np < 0){
-                                    continue;} else {break;}
-                            }
-                            eventSuccess[i] = 1;
-                        }
-
-                        // 4. if anyone feedback says unavailable or time illegal, modify time and jump to 1
-                        if (arrangement == 0){
-                            while (1){
-                                sleep(1);
-                                event* eventPtr = &eventArr[i];
-                                int modOutcome = modTime(++mod,eventPtr);
-                                if (modOutcome == 1 || mod > 37){
+                    // 3. if all feedbacks say available, tell child do and break this while
+                    if (arrangement == 1) {
+                        // for each child index c
+                        for (int j = 0; j < memberCount; ++j) {
+                            int c = teamArr[eventArr[i].teamID].member[j];
+                            // 1. tell child c to add
+                            write(f1fd[c][1], f1buf[c], 101);
+                            // 2. wait ack from C
+                            while (1) {
+                                np = read(cfd[c][0], cbuf[c], 101);
+                                if (np <= 0) {
+                                    continue;
+                                }
+                                if (cbuf[c][0] == 'D') {
                                     break;
                                 }
                             }
+                        }
+                        eventSuccess[i] = 1;
+                        break;
+                    } else {
+                        // 4. if anyone feedback says unavailable or time illegal, modify time and jump to 1
+                        sleep(1);
+                        event *eventPtr = &eventArr[i];
+                        int modOutcome = modTime(++mod, eventPtr);
+                        if (modOutcome == 1 || mod > 37) {
+                            break;
                         }
                     }
                 }
@@ -253,34 +278,43 @@ int main(int argc,char* argv[]) {
             for (int i = 0; i < 8; ++i) {
                 cbuf[i][0] = 'P';
                 cbuf[i][1] = 0;
-                write(cfd[i][1],cbuf,101);
+                write(cfd[i][1], cbuf, 101);
+
+                while (1) {
+                    // wait ack from C
+                    np = read(cfd[i][0], cbuf[i], 101);
+                    if (np <= 0) {
+                        continue;
+                    }
+                    if (cbuf[i][0] == 'D') {
+                        break;
+                    }
+                }
             }
         }
 
         // End: Tell every child to end process
-        if (signal == 'F'){
+        if (signal == 'F') {
             for (int i = 0; i < 8; ++i) {
                 cbuf[i][0] = 'F';
                 cbuf[i][1] = 0;
-                write(cfd[i][1],cbuf,101);
-				//maybe need to get response?
+                write(cfd[i][1], cbuf, 101);
             }
             break;
         }
-    }
 
 
 // close all pipe in the end
-    for (int i = 0; i < 8; ++i) {
-        close(f1fd[i][1]);
-        close(cfd[i][0]);
-    }
+        for (int i = 0; i < 8; ++i) {
+            close(f1fd[i][1]);
+            close(cfd[i][0]);
+        }
 
 // wait for all children
-    for (int i = 0; i < 8; ++i) {
-        wait(NULL);
+        for (int i = 0; i < 8; ++i) {
+            wait(NULL);
+        }
+
+        return 0;
     }
-
-    return 0;
 }
-
