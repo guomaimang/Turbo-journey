e6f9f905c2c2cd7d841544447a050acb03d6c36c
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-04-21 21:08:46 +0800

    update

---
+++ docs/docs/.vuepress/navbar.ts
@@ -0,0 +1,7 @@
+import { defineNavbarConfig } from "vuepress-theme-hope";
+
+export default defineNavbarConfig([
+  "/",
+  "/home",
+  { text: "Source", icon: "creative", link: "https://github.com/guomaimang/Turbo-journey/tree/main/Source" },
+]);
