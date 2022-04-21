8f828e8876392c55e1512d8765d1de76baa42f1f
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-04-21 21:11:33 +0800

    update

--- docs/docs/.vuepress/navbar.ts
+++ docs/docs/.vuepress/navbar.ts
@@ -2,6 +2,7 @@ import { defineNavbarConfig } from "vuepress-theme-hope";
 
 export default defineNavbarConfig([
   "/",
-  "/home",
+  { text: "Requirement", icon: "creative", link: "" },
   { text: "Source", icon: "creative", link: "https://github.com/guomaimang/Turbo-journey/tree/main/Source" },
+  { text: "About", icon: "creative", link: "" },
 ]);
